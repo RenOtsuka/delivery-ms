0b4ad6f18b23169d60fa261e66d71f15da8efbfe
Ryan <ryansantos.delossantos121@gmail.com>
2025-04-01 14:47:32 -0500

    first commit

---
+++ src/test/java/com/shipment/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shipment.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.delivery.ms.DeliveryMsApplication;
+
+@SpringBootTest(classes=DeliveryMsApplication.class)
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
